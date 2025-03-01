bb6d719413f4e04d4fd25d1afb025811f454d453
allivka <allivka1920@gmail.com>
2025-03-01 18:34:18 +0300

    added new exceptions and fixed one bug

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -3,10 +3,16 @@
 #define ADUL_EXCEPTIONS_HPP
 
 #include <exception>
+#include <string>
 
 namespace adul::exceptions {
     class Message;
+    
+    class FailedXMLFile;
+    class FailedReadingXMLFile;
+    class FailedWritingXMLFile;
 }
+
 
 class adul::exceptions::Message : std::exception {
 protected:
@@ -14,8 +20,36 @@ protected:
 public:
     Message();
     Message(const Message&) = default;
+    Message(const std::string&);
     Message(const char*);
     virtual const char* what() const noexcept override;
 };
+
+    
+class adul::exceptions::FailedXMLFile : std::exception {
+protected:
+    std::string path;
+public:
+    FailedXMLFile() = default;
+    FailedXMLFile(const char* p_path);
+    FailedXMLFile(const std::string& p_path);
+    FailedXMLFile(const FailedXMLFile&) = default;
+    virtual const char* what() const noexcept override;
+};
+
+
+class adul::exceptions::FailedReadingXMLFile : protected FailedXMLFile {
+public:
+    using FailedXMLFile::FailedXMLFile;
+    virtual const char* what() const noexcept override;
+};
+
+
+class adul::exceptions::FailedWritingXMLFile : FailedXMLFile {
+public:
+    using FailedXMLFile::FailedXMLFile;
+    virtual const char* what() const noexcept override;
+};
+
 
 #endif
--- sources/tinyxml2.cpp
+++ sources/tinyxml2.cpp
@@ -130,7 +130,7 @@ static const unsigned char TIXML_UTF_LEAD_0 = 0xefU;
 static const unsigned char TIXML_UTF_LEAD_1 = 0xbbU;
 static const unsigned char TIXML_UTF_LEAD_2 = 0xbfU;
 
-namespace ADUL {
+namespace adul {
 
 namespace xml
 {

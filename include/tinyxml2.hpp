e761625ec2bef1e3ed1cf772bebe6f7f11f2a979
allivka <allivka1920@gmail.com>
2025-03-01 17:09:08 +0300

    small fix

--- include/tinyxml2.hpp
+++ include/tinyxml2.hpp
@@ -111,7 +111,7 @@ static const int TIXML2_PATCH_VERSION = 0;
 // so there needs to be a limit in place.
 static const int TINYXML2_MAX_ELEMENT_DEPTH = 500;
 
-namespace ADUL {
+namespace adul {
 
 namespace xml
 {
